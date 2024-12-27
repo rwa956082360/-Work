ae1b7c0fde070aca93b404aa6d44397a14f3b0af
วาาาา <rwa7414@gmail.com>
2024-12-27 14:49:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/-Work" element={<Home />} />
-                    <Route path="/Home" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/courses" element={<Courses />} />
